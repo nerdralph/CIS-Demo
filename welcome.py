5ccdb3dcedce0577ca37ebea3f60c8a2f78e9835
Nerd Ralph <ralphdoncaster@gmail.com>
2018-07-05 19:02:32 -0300

    Tweak message

--- welcome.py
+++ welcome.py
@@ -23,7 +23,7 @@ def Welcome():
 
 @app.route('/myapp')
 def WelcomeToMyapp():
-    return 'Welcome again to my app running on Bluemix!'
+    return 'Welcome to my World!'
 
 @app.route('/api/people')
 def GetPeople():
